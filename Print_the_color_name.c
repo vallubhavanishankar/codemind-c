1493486c2e6a061fad8ff884470073a06b3f1c26
vallubhavanishankar <119730721+vallubhavanishankar@users.noreply.github.com>
2022-12-10 14:16:49 +0530

    #include<stdio.h> int main() {     char chhichhore,V,I,B,G,Y,O,R;     scanf("%c",&chhichhore);     if(chhichhore==V)     {         printf("Violet");     }     else if(chhichhore=='I')     {         printf("Indigo");     }     else if(chhichhore=='B')     {         printf("Blue");     }     else if(chhichhore=='G')     {         printf("Green");     }     else if(chhichhore=='Y')     {         printf("Yellow");     }     else if(chhichhore=='O')     {         printf("Orange");     }     else if(chhichhore=='R')     {         printf("Red");     }     else     {         printf("-1");     } }

---
+++ Print_the_color_name.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+int main()
+{
+    char chhichhore,V,I,B,G,Y,O,R;
+    scanf("%c",&chhichhore);
+    if(chhichhore==V)
+    {
+        printf("Violet");
+    }
+    else if(chhichhore=='I')
+    {
+        printf("Indigo");
+    }
+    else if(chhichhore=='B')
+    {
+        printf("Blue");
+    }
+    else if(chhichhore=='G')
+    {
+        printf("Green");
+    }
+    else if(chhichhore=='Y')
+    {
+        printf("Yellow");
+    }
+    else if(chhichhore=='O')
+    {
+        printf("Orange");
+    }
+    else if(chhichhore=='R')
+    {
+        printf("Red");
+    }
+    else 
+    {
+        printf("-1");
+    }
+}
