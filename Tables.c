1e7b8fb7134e48bb91d1fe1b5c7891fe893383b9
vallubhavanishankar <119730721+vallubhavanishankar@users.noreply.github.com>
2022-12-21 22:34:58 +0530

    #include<stdio.h> int main() {     int a,b,i,r;     scanf("%d%d",&a,&b);     for(i=1;i<=b;i++)     {         if(i%2!=0)         {             r=a*i;             printf("%d x %d = %d ",a,i,r);         }     } }

---
+++ Tables.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,i,r;
+    scanf("%d%d",&a,&b);
+    for(i=1;i<=b;i++)
+    {
+        if(i%2!=0)
+        {
+            r=a*i;
+            printf("%d x %d = %d
+",a,i,r);
+        }
+    }
+}
