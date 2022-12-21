1872c0e666f3b7a200d8d31f48ec4dfbda563c30
vallubhavanishankar <119730721+vallubhavanishankar@users.noreply.github.com>
2022-12-21 20:07:05 +0530

    #include<stdio.h> int main() {     int i,bittu;     scanf("%d",&bittu);     for(i=1;i<=bittu;i++)     {         int a,b;         scanf("%d%d",&a,&b);         int c;         c=a+b;         printf("%d ",c);     } }

---
+++ sum_of_the_given_numbers.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int i,bittu;
+    scanf("%d",&bittu);
+    for(i=1;i<=bittu;i++)
+    {
+        int a,b;
+        scanf("%d%d",&a,&b);
+        int c;
+        c=a+b;
+        printf("%d
+",c);
+    }
+}
